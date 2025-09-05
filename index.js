e2d0e4d626ef605c60a183f7a5a685be3021893b
yehuda-d <yehuda130@gmail.com>
2025-09-05 13:42:43 +0300

    added capibility to upload and edit files

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const port = 3500;
 app.use(express.static(path.join(__dirname,'public')));
+// app.use(express.json());
+
 
 //Routers
 app.get('/',(req,res)=>{
