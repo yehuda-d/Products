12219612abbb72657d85ee5b0581721cfe77db20
yehuda-d <yehuda130@gmail.com>
2025-09-05 11:23:56 +0300

    added empty array to server

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const path = require('path');
 const app = express();
 const port = 3500;
 
+const products = [];
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html')
 });
+
+app.get('/products',(req,res)=>{
+    res.status(200).json(products)
+})
 
 app.listen(port,()=>{console.log(`http://localhost:${port}`)});
